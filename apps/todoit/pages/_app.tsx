0ae9b18c3cf304b458c67c20defeb3dcfe55da50
Victor Avila <vicav99@gmail.com>
2022-02-21 22:39:54 -0700

    feat: render initial state example

--- apps/todoit/pages/_app.tsx
+++ apps/todoit/pages/_app.tsx
@@ -1,18 +1,18 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import { Provider } from 'react-redux';
 import './styles.css';
+import { wrapper } from '../redux/store';
 
-function CustomApp({ Component, pageProps }: AppProps) {
+function CustomApp({ Component, pageProps, ...props }: AppProps & any) {
   return (
     <>
       <Head>
         <title>Welcome to todoit!</title>
       </Head>
-      <main className="app">
-        <Component {...pageProps} />
-      </main>
+      <Component {...pageProps} />
     </>
   );
 }
 
-export default CustomApp;
+export default wrapper.withRedux(CustomApp);
